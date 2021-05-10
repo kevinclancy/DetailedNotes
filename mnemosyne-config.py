4d87e0e28532b795d16a2a5af0890c279d724230
Kevin Clancy <kclanc@gmail.com>
2021-05-10 10:22:11 -0500

    updated mnemosyne cards

--- mnemosyne-config.py
+++ mnemosyne-config.py
@@ -62,6 +62,33 @@ latex_preamble = r"""
 }
 \usetikzlibrary{cd}
 
+%%% quiver.sty %%%
+% `tikz-cd` is necessary to draw commutative diagrams.
+\RequirePackage{tikz-cd}
+% `amssymb` is necessary for `\lrcorner` and `\ulcorner`.
+\RequirePackage{amssymb}
+% `calc` is necessary to draw curved arrows.
+\usetikzlibrary{calc}
+% `pathmorphing` is necessary to draw squiggly arrows.
+\usetikzlibrary{decorations.pathmorphing}
+
+% A TikZ style for curved arrows of a fixed height, due to AndréC.
+\tikzset{curve/.style={settings={#1},to path={(\tikztostart)
+    .. controls ($(\tikztostart)!\pv{pos}!(\tikztotarget)!\pv{height}!270:(\tikztotarget)$)
+    and ($(\tikztostart)!1-\pv{pos}!(\tikztotarget)!\pv{height}!270:(\tikztotarget)$)
+    .. (\tikztotarget)\tikztonodes}},
+    settings/.code={\tikzset{quiver/.cd,#1}
+        \def\pv##1{\pgfkeysvalueof{/tikz/quiver/##1}}},
+    quiver/.cd,pos/.initial=0.35,height/.initial=0}
+
+% TikZ arrowhead/tail styles.
+\tikzset{tail reversed/.code={\pgfsetarrowsstart{tikzcd to}}}
+\tikzset{2tail/.code={\pgfsetarrowsstart{Implies[reversed]}}}
+\tikzset{2tail reversed/.code={\pgfsetarrowsstart{Implies}}}
+% TikZ arrow styles.
+\tikzset{no body/.style={/tikz/dash pattern=on 0 off 1mm}}
+%%% quiver.sty %%%
+
 \newcommand{\inconsist}{\mathrel{\substack{\smile \\ \frown}}}
 \newcommand{\consist}{\mathrel{\substack{\frown \\ \smile}}}
 
@@ -81,9 +108,14 @@ latex_preamble = r"""
 \newcommand{\srangle}{\scalebox{0.5}{$~\rangle$}}
 \newcommand{\chan}{~\stackinset{l}{.5ex}{c}{}{\scalebox{0.6}{$\circ$}}{$\to$}~}
 \newcommand{\dotto}{~\stackinset{l}{}{c}{}{$\bullet$}{$\to$}~}
+\newcommand{\dotminus}{~\stackinset{c}{}{c}{}{\raisebox{1.2ex}{$\cdot$}}{$-$}~}
 \newcommand{\antiphi}{~\stackinset{l}{}{c}{}{$\backslash$}{$o$}~}
 \newcommand{\absepi}{~\stackinset{r}{}{c}{}{\scalebox{0.5}{$\triangleright$}}{$-$}~}
 \newcommand{\absmono}{\mapsto}
+\newcommand{\wand}{~\stackinset{r}{}{c}{}{\scalebox{0.9}{$\ast$}}{$-$}~}
+\newcommand{\dashdot}{~\stackinset{r}{}{c}{}{\scalebox{0.9}{$\bullet$}}{$-$}~}
+\newcommand{\dotdash}{~\stackinset{l}{}{c}{}{\scalebox{0.9}{$\bullet$}}{$-$}~}
+
 
 \newcommand{\vrt}[2]{
 \pile{
